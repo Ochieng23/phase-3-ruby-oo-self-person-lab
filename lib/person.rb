90adcaaedf729ba802192b9289e9dfde4d379b02
+ <+>
2023-02-13 02:56:55 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -31,43 +31,37 @@ class Person
     end 
   
     def take_bath
-        increment = [4, 10 - @hygiene].min
-        @hygiene = [[0, @hygiene + increment].max, 10].min
+        self.hygiene += 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
       end
   
     def work_out
-      decrement = [3, @hygiene].min
-      @hygiene -= decrement
-      @happiness = [[0, @happiness + [2, 10 - @happiness].min].max, 10].min
-      '♪ another one bites the dust ♫'
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
     end
   
     def call_friend(friend)
-      "Hi Felix! It's Penelope. How are you?"
-      increment = [3, 10 - @happiness].min
-      friend_increment = [3, 10 - friend.happiness].min
-      self.happiness += increment
-      friend.happiness += friend_increment
+        self.happiness += 3
+        friend.happiness +=3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
     end 
   
     def start_conversation(friend, topic)
-      case topic
-      when "politics"
-        'blah blah partisan blah lobbyist'
-        decrement = [2, @happiness].min
-        friend_decrement = [2, friend.happiness].min
-        self.happiness -= decrement
-        friend.happiness -= friend_decrement
-      when "weather" 
-        'blah blah sun blah rain'
-        increment = [1, 10 - @happiness].min
-        friend_increment = [1, 10 - friend.happiness].min
-        self.happiness += increment
-        friend.happiness += friend_increment
-      else
-        "blah blah blah blah blah"
-      end
-    end 
+        if topic === "politics"
+            self.happiness -= 2
+            friend.happiness -=2
+            "blah blah partisan blah lobbyist"
+        elsif topic === "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            self.happiness +=0
+            friend.happiness += 0
+            "blah blah blah blah blah"
+        end
+    end
+    
   end
   
